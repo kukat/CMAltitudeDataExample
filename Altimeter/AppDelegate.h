234669b4beecc2e9f1e4d0a95bec93c7225dcda2
Cheng Yao <kukathe@gmail.com>
2015-03-25 02:04:03 +0800

    Initial Commit

---
+++ Altimeter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Altimeter
+//
+//  Created by Cheng Yao on 25/3/15.
+//  Copyright (c) 2015 Cheng Yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Altimeter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Altimeter
+//
+//  Created by Cheng Yao on 25/3/15.
+//  Copyright (c) 2015 Cheng Yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
